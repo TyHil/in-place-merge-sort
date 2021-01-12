1fd471dbbe9375796dd1bf60557f3709a697e85b
Tyler Hill <gotyhill@gmail.com>
2021-01-12 16:21:29 -0600

    Extra std:: in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ void mergeSort(int * x, const int min, const int max) {
   if (mid - min > 1) mergeSort(x, min, mid);//split bottom
   if (max - mid > 1) mergeSort(x, mid, max);//split top
   if (max > min) {//merge
-    //if (max-min>100) cout << "Merging: min: " << min << " max: " << max << " max-min: " << max-min << '\n';//progress print only displays merges larger than 100 data points
+    //if (max-min>100) std::cout << "Merging: min: " << min << " max: " << max << " max-min: " << max-min << '\n';//progress print only displays merges larger than 100 data points
     int p1 = min, p2 = mid, p3 = mid;
     while (p1 < p2 and p3 < max) {
       if (x[p1] < x[p3]) {//use < for ascending and > for descending
